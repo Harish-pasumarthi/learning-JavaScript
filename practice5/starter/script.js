37582ade1d34b8eb66e52003d3285120d314bee7
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-02-19 15:11:42 +0530

    practice code of DOM

--- practice5/starter/script.js
+++ practice5/starter/script.js
@@ -16,3 +16,36 @@ document.querySelector('.check').addEventListener('click', function () {
 //SYNTAX---->  addEventListener('event', function());
 */
 //GAME LOGIC
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+const checkNumber = function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '📛 No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🏆 Correct Answer';
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👎🏻 You Lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👎🏻 You Lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+};
+
+document.querySelector('.check').addEventListener('click', checkNumber);
