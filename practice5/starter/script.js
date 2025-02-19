c1af17479d8e18fc777923bec9645a2a2c3c6233
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-02-19 08:37:54 +0530

    adding the practice code of JS

---
+++ practice5/starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '⚡Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 3;
+
+document.querySelector('.guess').value = 5;
+console.log(document.querySelector('.guess').value);
+*/
