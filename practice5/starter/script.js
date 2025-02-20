c706d933a8ddab6a1c8fc5f43a613ea47bbff8cb
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-02-20 23:08:51 +0530

    added sound effects to the web Game

--- practice5/starter/script.js
+++ practice5/starter/script.js
@@ -1,20 +1,5 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '⚡Correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 3;
-
-document.querySelector('.guess').value = 5;
-console.log(document.querySelector('.guess').value);
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
-  console.log(typeof guess);
-});
-//SYNTAX---->  addEventListener('event', function());
-*/
 //GAME LOGIC
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
