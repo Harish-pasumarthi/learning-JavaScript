62791cf12a741a2317efade896a80541a26e3823
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-02-20 23:26:31 +0530

    added sound effects to the web Game

--- practice5/starter/script.js
+++ practice5/starter/script.js
@@ -4,27 +4,27 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
+//playSound function for all sounds
+const playSound = className => {
+  const audio = document.querySelector(className);
+  audio.play();
+};
+//displayMessage function for .message class eg:high, low, no number etc.
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
+//function checks the guess number correct or wrong
 const checkNumber = function () {
   const guess = Number(document.querySelector('.guess').value);
-
-  //playSound function for all sounds
-  const playSound = className => {
-    const audio = document.querySelector(className);
-    audio.play();
-  };
-
   //When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '🚫 No Number';
+    displayMessage('🚫 No Number');
     document.querySelector('.guess').style.backgroundColor = '#cf0000';
-    // Play the sound:
-    playSound('.noNumberSound');
-    //-------------------------------------------------
-
-    //when player wins
+    playSound('.noNumberSound'); // Play the sound
+    //when the guess is correct
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🏆 Correct Answer';
+    displayMessage('🏆 Correct Answer');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#2dde12';
     document.querySelector('.number').style.width = '30rem';
@@ -32,11 +32,7 @@ const checkNumber = function () {
     //hightscore logic
     if (score > highScore) highScore = score;
     document.querySelector('.highscore').textContent = highScore;
-
-    // Play the sound:
-    playSound('.winSound');
-    //-------------------------------------------------
-
+    playSound('.winSound'); // Play the sound:
     //when guess is wrong
   } else if (guess !== secretNumber) {
     document.querySelector('.guess').style.backgroundColor = '';
@@ -46,12 +42,10 @@ const checkNumber = function () {
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '👎🏻 You Lost the game!';
+      displayMessage('👎🏻 You Lost the game!');
       document.querySelector('body').style.backgroundColor = '#cf0000';
       document.querySelector('.score').textContent = 0;
-      // Play the sound:
-      playSound('.lostSound');
-      //-------------------------------------------------
+      playSound('.lostSound'); // Play the sound:
     }
   }
 };
@@ -63,16 +57,12 @@ const resetGame = function () {
   score = 20;
   document.querySelector('.score').textContent = score;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.guess').style.backgroundColor = '';
   document.querySelector('.guess').value = null;
-
-  // Play the sound:
-  const newGame = document.querySelector('.newGame');
-  newGame.play();
-  //-------------------------------------------------
+  playSound('.newGame'); // Play the sound:
 };
 document.querySelector('.again').addEventListener('click', resetGame);
