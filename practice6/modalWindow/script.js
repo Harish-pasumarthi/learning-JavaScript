7be9f51e682b3bca0c6a329aa38c8ffeefe05f74
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-02-21 10:55:11 +0530

    adding the practice code of JS

--- practice6/modalWindow/script.js
+++ practice6/modalWindow/script.js
@@ -1,1 +1,5 @@
 'use strict';
+const moadal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpeneModal = document.querySelectorAll('.show-modal');
