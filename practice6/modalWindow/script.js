a7267c44b02e0e7002ba739ff6859fa42863d5a0
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-02-24 05:05:44 +0530

    updated

--- practice6/modalWindow/script.js
+++ practice6/modalWindow/script.js
@@ -1,5 +1,5 @@
 'use strict';
-const moadal = document.querySelector('.modal');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpeneModal = document.querySelectorAll('.show-modal');
