2dde009f4e42909ea49f2d79674064d6039014a1
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-02-25 11:25:55 +0530

    adding the practice code of JS

--- practice6/modalWindow/script.js
+++ practice6/modalWindow/script.js
@@ -2,4 +2,29 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpeneModal = document.querySelectorAll('.show-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const escPress = function (event) {
+  if (event.key === 'Escape') {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', escPress);
