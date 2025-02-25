fe4db98361cad25d949d8a1b2dbaf7adc0c9721f
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-02-26 04:03:19 +0530

    modified

--- practice7-Pig-Game/starter/script.js
+++ practice7-Pig-Game/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+//slecting Elements
+const score0El = document.querySelector('#score--0');
+const current0El = document.querySelector('#current--0');
+const score1El = document.querySelector('#score--1');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random Dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3.Check for rolled 1: if true
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // switch to next player
+    currentScore = 0;
+  }
+});
