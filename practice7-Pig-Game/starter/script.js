c0fea8e872abb15994a71cd5e4f0fa1c6591eec5
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-02-26 05:17:58 +0530

    modified

--- practice7-Pig-Game/starter/script.js
+++ practice7-Pig-Game/starter/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 //slecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const current0El = document.querySelector('#current--0');
 const score1El = document.querySelector('#score--1');
@@ -31,9 +34,16 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to next player
     currentScore = 0;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    //changing color to the switched player
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
