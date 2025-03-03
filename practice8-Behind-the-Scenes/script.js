81c5ac0f0398984439a82719da6ecd6ff47f64ff
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-03-03 10:57:57 +0530

    updated Js code

--- practice8-Behind-the-Scenes/script.js
+++ practice8-Behind-the-Scenes/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-function outerFunction(x) {
-  return function innerFunction(y) {
-    return x + y;
-  };
-}
+// function outerFunction(x) {
+//   return function innerFunction(y) {
+//     return x + y;
+//   };
+// }
 
-let add5 = outerFunction(5); // add5 now holds innerFunction
-console.log(add5);
+// let add5 = outerFunction(5); // add5 now holds innerFunction
+// console.log(add5);
 
 // console.log(add5(3)); // Output: 8 (5 + 3)
 // console.log(add5(10)); // output: 15 (5 + 10)
