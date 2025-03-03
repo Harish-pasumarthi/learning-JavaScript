97ea40bb0f36898e679f8c942bb603670180c21b
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-03-03 10:11:27 +0530

    new practice folder

--- practice8-Behind-the-Scenes/script.js
+++ practice8-Behind-the-Scenes/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function outerFunction(x) {
+  function innerFunction(y) {
+    return x + y;
+  }
+  return innerFunction;
+}
+
+const add5 = outerFunction(5); // add5 now holds innerFunction
+console.log(add5(3)); // Output: 8 (5 + 3)
+console.log(add5(10)); // output: 15 (5 + 10)
