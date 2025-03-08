e07478c1b47fa9aba97ccd8b7ced0526c6c0264d
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-03-08 12:30:38 +0530

    modified

--- practice8-Behind-the-Scenes/script.js
+++ practice8-Behind-the-Scenes/script.js
@@ -11,9 +11,36 @@
 
 // console.log(add5(3)); // Output: 8 (5 + 3)
 // console.log(add5(10)); // output: 15 (5 + 10)
-hell();
-function hell() {
-  console.log('hi');
-}
+// hell();
+// function hell() {
+//   console.log('hi');
+// }
 
-console.log(this);
+// console.log(this);
+
+// const obj1 = {
+//   name: 'obj1',
+//   func: function () {
+//     console.log(this);
+//   },
+// };
+// const obj2 = {
+//   age: 23,
+// };
+
+// obj2.func = obj1.func;
+
+// obj2.func();
+// obj1.func()
+
+const fun = function () {
+  console.log(this);
+};
+
+fun();
+
+const fun1 = () => {
+  console.log(this);
+};
+
+fun1();
