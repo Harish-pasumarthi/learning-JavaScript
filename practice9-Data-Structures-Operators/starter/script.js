b05b992856e6e2c30a10d2cda06d35baf5c4c9f7
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-03-24 22:23:13 +0530

    adding the practice code of JS

--- practice9-Data-Structures-Operators/starter/script.js
+++ practice9-Data-Structures-Operators/starter/script.js
@@ -314,6 +314,26 @@ const books = [
 
 console.log(`The Lenght of the Books is : ${books.length}`);
 
+//Destructure the first book object from the books array into variables called title, author and ISBN.
+// const [book1] = books;
+// const { title, author, ISBN } = book1;
+// console.log(title, author, ISBN);
+
+//Each book object has the keywords property. Destructure the first book object from the books array into a variable called tags. The tags variable should be assigned with the value of the keywords property.
+// const { keywords: tags } = books[0];
+
+// The seventh book from the books array is missing the programmingLanguage property. Destructure the seventh book object (books[6]) into variables called language and programmingLanguage. Assign the programmingLanguage variable with a default value of 'unknown'.
+// const { language, programmingLanguage = 'unknown' } = books[6];
+// console.log(language, programmingLanguage);
+
+// Below are two variables called bookTitle and bookAuthor. Reassign them with the values of the title and author properties of the first book object from the books array.
+// let bookTitle = 'unknown';
+// let bookAuthor = 'unknown';
+// ({ title: bookTitle, author: bookAuthor } = books[0]);
+// console.log(bookTitle, bookAuthor);
+
+//----------------------------------------------------------------
+
 //Destructure the books array into two variables called firstBook and secondBook.
 // const [firstbook, secondBook] = books;
 
