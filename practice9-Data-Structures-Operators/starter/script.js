0d386e92483ff90192eb979ede72b36b3299d1ea
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-04-26 17:29:20 +0530

    Updated Js code

--- practice9-Data-Structures-Operators/starter/script.js
+++ practice9-Data-Structures-Operators/starter/script.js
@@ -661,19 +661,42 @@ Let's continue with our football betting app! This time, we have a map with a lo
 GOOD LUCK 😀
 */
 
-const gameEvents = new Map([
-  [17, '⚽️ GOAL'],
-  [36, '🔁 Substitution'],
-  [47, '⚽️ GOAL'],
-  [61, '🔁 Substitution'],
-  [64, '🔶 Yellow card'],
-  [69, '🔴 Red card'],
-  [70, '🔁 Substitution'],
-  [72, '🔁 Substitution'],
-  [76, '⚽️ GOAL'],
-  [80, '⚽️ GOAL'],
-  [92, '🔶 Yellow card'],
-]);
+// const gameEvents = new Map([
+//   [17, '⚽️ GOAL'],
+//   [36, '🔁 Substitution'],
+//   [47, '⚽️ GOAL'],
+//   [61, '🔁 Substitution'],
+//   [64, '🔶 Yellow card'],
+//   [69, '🔴 Red card'],
+//   [70, '🔁 Substitution'],
+//   [72, '🔁 Substitution'],
+//   [76, '⚽️ GOAL'],
+//   [80, '⚽️ GOAL'],
+//   [92, '🔶 Yellow card'],
+// ]);
 
-const events = new Set([...gameEvents.values()]);
-console.log(events);
+// const events = new Set([...gameEvents.values()]);
+// console.log(events);
+
+// gameEvents.delete(64);
+// console.log(gameEvents.has(64));
+
+// console.log(gameEvents.size);
+
+// console.log(
+//   `An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
+
+// for (const event of events) {
+//   if (events.keys() > 45) {
+//     console.log(`[FIRST HALF] : ${event}`);
+//   } else {
+//     console.log(`[SECOND HALF]: ${event}`);
+//   }
+// }
+
+//CORRECT ANSWER FOR THE 4 QUESTION
+// for (const [min, event] of gameEvents) {
+//   const half = min <= 45 ? 'FIRST' : 'SECOND';
+//   console.log(`[${half} HALF] ${min}: ${event}`);
+// }
