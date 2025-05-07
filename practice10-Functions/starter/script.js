80f8f8473eaaf753b6fc6e8144bdfc91998262f9
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-05-07 22:47:11 +0530

    Functions Updated code

--- practice10-Functions/starter/script.js
+++ practice10-Functions/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const bookings = [];
+const createBookings = function (a = 'A1', b = 'hi', c = 'rama') {
+  const booking = {
+    a,
+    b,
+    c,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+console.log(bookings);
+createBookings();
