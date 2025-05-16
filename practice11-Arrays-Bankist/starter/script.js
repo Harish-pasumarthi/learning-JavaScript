b9855608e1266d4d1c0b1f6720b5af2512788506
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-05-16 09:32:31 +0530

    updated code

--- practice11-Arrays-Bankist/starter/script.js
+++ practice11-Arrays-Bankist/starter/script.js
@@ -99,7 +99,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // const eurToUsd = 1.1;
 // const movementsUSD = movements.map(mov => mov * eurToUsd);
