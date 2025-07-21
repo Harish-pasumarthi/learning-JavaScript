d7039b9816025bce17129f6ff56688e9aedd2c6a
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-07-21 12:46:19 +0530

    updated code

--- practice13-Advanced-DOM-Bankist/starter/script.js
+++ practice13-Advanced-DOM-Bankist/starter/script.js
@@ -102,3 +102,31 @@ tabsContainer.addEventListener('click', function (e) {
     .classList.add('operations__content--active');
 });
 //-------------------------------------------------------------------
+
+//-----------------------------------------------------------------
+//MENU FADE ANIMATION
+const nav = document.querySelector('.nav');
+
+nav.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    console.log(link);
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = 0.5;
+      }
+    });
+  }
+});
+
+nav.addEventListener('mouseout', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    console.log(link);
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = 1;
+    });
+  }
+});
