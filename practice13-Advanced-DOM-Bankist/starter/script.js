7165bfdfa3506711cd18506c2c4ca164081b1539
Harish Pasumarthi <pasumarthiharish33@gmail.com>
2025-07-25 19:01:05 +0530

    updated code

--- practice13-Advanced-DOM-Bankist/starter/script.js
+++ practice13-Advanced-DOM-Bankist/starter/script.js
@@ -155,4 +155,21 @@ nav.addEventListener('mouseout', function (e) {
 
 // const observer = new IntersectionObserver(obsCallback, obsOptions);
 // observer.observe(section1);
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+//callback function
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
 //--------------------------------------
